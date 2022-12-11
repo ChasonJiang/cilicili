e18193ce68b3bf5707e72639376dd13728305b2f
ChasonJiang <1440499136@qq.com>
2022-12-11 13:07:32 +0800

    modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -22,15 +22,15 @@ from player.CiliCiliPlayer import CiliCiliPlayer
 #     sys.exit(sr.exec_())
 
 
-vurl = "http://v16m-default.akamaized.net/1b0a5e06c655f4e6bbb6ee1795b40853/638f69fc/video/tos/alisg/tos-alisg-v-0000/ogQeorDzb1IfAQnGaeTIbdjF4TTGODzAgktoCv/?a=2011&ch=0&cr=0&dr=0&net=5&cd=0%7C0%7C0%7C0&br=4534&bt=2267&cs=0&ds=4&ft=XE5bCqT0mmjPD12e4oyq3wU7C1JcMeF~OD&mime_type=video_mp4&qs=0&rc=NDY6ZDc6ZTtmN2g6OTk1NEBpM2hrajw6Zm1nZzMzODYzNEAvYGAuMmA1X2ExLy4yYl82YSNyMWhqcjRnNnNgLS1kMC1zcw%3D%3D&l=2022120609495901019204616019076160&btag=80000"
+# vurl = "http://v16m-default.akamaized.net/1b0a5e06c655f4e6bbb6ee1795b40853/638f69fc/video/tos/alisg/tos-alisg-v-0000/ogQeorDzb1IfAQnGaeTIbdjF4TTGODzAgktoCv/?a=2011&ch=0&cr=0&dr=0&net=5&cd=0%7C0%7C0%7C0&br=4534&bt=2267&cs=0&ds=4&ft=XE5bCqT0mmjPD12e4oyq3wU7C1JcMeF~OD&mime_type=video_mp4&qs=0&rc=NDY6ZDc6ZTtmN2g6OTk1NEBpM2hrajw6Zm1nZzMzODYzNEAvYGAuMmA1X2ExLy4yYl82YSNyMWhqcjRnNnNgLS1kMC1zcw%3D%3D&l=2022120609495901019204616019076160&btag=80000"
 
 
 def Application(args:list):
     logging.basicConfig(format='--> %(levelname)s : %(message)s', level=logging.DEBUG, force=True) # DEBUG
-    media_info = MediaInfo([vurl],[vurl],"network")
+    # media_info = MediaInfo([vurl],[vurl],"network")
     # media_info=MediaInfo(["assets\\5s.mkv", "assets\\5s.mkv"],["assets\\5s.mkv", "assets\\5s.mkv"],"file")
     # media_info = MediaInfo([r"C:\Users\White\Project\rtsr_client_pyqt\assets\[Kamigami&Mabors] Saenai Heroine no Sodatekata Flat - 00 [1080p x265 Ma10p AAC].mkv"],[r"C:\Users\White\Project\rtsr_client_pyqt\assets\[Kamigami&Mabors] Saenai Heroine no Sodatekata Flat - 00 [1080p x265 Ma10p AAC].mkv"],"file")
-    # media_info = MediaInfo(["assets\\360p.mkv"],["assets\\360p.mkv"],"file")
+    media_info = MediaInfo(["assets\\360p.mkv"],["assets\\360p.mkv"],"file")
     srContext = args
     app = QApplication(sys.argv)
     # player = BasePlayer()
